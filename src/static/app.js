54a5aaa2cf413fd6bcba4a4fa40d72e7ea555a70
sivasusheendart <sivasusheendar@gmail.com>
2025-12-12 09:50:47 +0000

    Adding Participants

--- src/static/app.js
+++ src/static/app.js
@@ -25,6 +25,18 @@ document.addEventListener("DOMContentLoaded", () => {
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
           <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
+          <div class="activity-card-participants">
+            <div class="activity-card-participants-title">Participants:</div>
+            <ul class="activity-card-participants-list">
+              ${
+                details.participants.length > 0
+                  ? details.participants.map(
+                      (participant) => `<li>${participant}</li>`
+                    ).join("")
+                  : `<li><em>No participants yet</em></li>`
+              }
+            </ul>
+          </div>
         `;
 
         activitiesList.appendChild(activityCard);
